b38889d2cb5284befbf6de06f86bb64463ae1ef6
Will Nilges <wdn5796@rit.edu>
2023-02-02 21:25:25 -0500

    NVML

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use nvml_wrapper::Nvml;
 fn main() {
     println!("Hello, world!");
+    let nvml = Nvml::init().unwrap();
+    let nvml_device_count = nvml.device_count().unwrap();
+    println!("We have {} devices.", nvml_device_count); 
 }
