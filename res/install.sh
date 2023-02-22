bf72b3fb509eac6a1dd118c2f2fead429d8be2fe
Ethan Ferguson <ethanf108@gmail.com>
2023-02-21 21:56:29 -0500

    Added setuid (maybe a bad idea)

--- res/install.sh
+++ res/install.sh
@@ -3,5 +3,6 @@
 set -e
 
 cargo build --release
-sudo install target/release/clobber /usr/local/sbin; echo "Installed clobber."
+sudo install target/release/clobber /usr/local/sbin && chmod u+s /usr/local/sbin/clobber && echo "Installed clobber."
+sudo install target/release/clobberd /usr/local/sbin && echo "Installed clobberd."
 sudo cp res/clobber.sh /etc/profile.d; echo "Added profile.d script."
